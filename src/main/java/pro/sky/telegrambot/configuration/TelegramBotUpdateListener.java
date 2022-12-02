39de653a25fcfb70f4e07b20f59ee5c09845fd94
igr1 <dfrgth>
2022-12-03 09:06:15 +1000

    bot proseed

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotUpdateListener.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotUpdateListener.java
@@ -1,6 +1,8 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -10,15 +12,22 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.List;
+
 
 @Component
-public class TelegramBotUpdateListener {
+public class TelegramBotUpdateListener implements UpdatesListener {
     @Autowired
     TelegramBot telegramBot;
 
     public  TelegramBotUpdateListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
         this.telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> list) {
+        return 0;
     }
 
    /*@EventListener({ContextRefreshedEvent.class})
